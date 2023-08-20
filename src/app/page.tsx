05e8acb0aed22c4d7496fbd83b49accdf632bb35
Kalle Moen <hello@kallemoen.com>
2023-08-20 12:33:02 +0100

    Added headline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
-    <h1>FriendTech Alpha</h1>
+    <h1 className="text-4xl font-bold left-aligned mt-8 mb-4">FriendTech.info</h1>
     <div className="flex items-center justify-center w-2/3">
       <BasicTable people={data} />
     </div>
