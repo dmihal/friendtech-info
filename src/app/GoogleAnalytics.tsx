7580313f0f7ae04e6b058a2e5cb6235a8792a419
David Mihal <david@dmihal.com>
2023-08-20 14:46:50 +0100

    Gtag page loads

--- src/app/GoogleAnalytics.tsx
+++ src/app/GoogleAnalytics.tsx
@@ -1,7 +1,41 @@
 "use client";
+import { usePathname } from "next/navigation";
 import Script from "next/script";
+import { useEffect, useRef } from "react";
+
+
+const pageview = (url: URL) => {
+  // @ts-ignore
+  window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string, {
+    page_path: url,
+  });
+};
+
+export const useGtag = () => {
+  const pathname = usePathname(); // Get current route
+
+  // Save pathname on component mount into a REF
+  const savedPathNameRef = useRef(pathname);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'development') return;
+
+    const handleRouteChange = (url: URL) => {
+      pageview(url);
+    };
+
+    if (savedPathNameRef.current !== pathname) {
+      handleRouteChange(new URL(pathname, window.location.origin));
+      // Update REF
+      savedPathNameRef.current = pathname;
+    }
+  }, [pathname]);
+};
+
 
 export const GoogleAnalytics = ({ GA_TRACKING_ID }: { GA_TRACKING_ID: string }) => {
+  useGtag()
+
   return (
     <>
       <Script
