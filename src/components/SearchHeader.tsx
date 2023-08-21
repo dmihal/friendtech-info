87b3c4bd7717562c80258b8b99f02260ddb24924
Kalle Moen <hello@kallemoen.com>
2023-08-21 12:12:11 +0100

    Fixed mobile styling for search

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -9,15 +9,17 @@ import { useState } from 'react';
 export default function SearchHeader() {
   const [searchOpen, setSearchOpen] = useState(false)
   return (
-    <div className="border-b border-gray-200 pb-5 sm:flex w-full sm:items-center sm:justify-between">
-      <h3 className="text-xl font-semibold leading-6 text-gray-900">Top Friend.tech users</h3>
+    <div className="border-b border-gray-200 pb-5 md:flex w-full sm:items-center sm:justify-between">
+      <h3 className="text-xl font-semibold leading-6 text-gray-900 mb-3">Top Friend.tech users</h3>
       <button
           type="button"
           onClick={() => setSearchOpen(true)}
-          className="flex items-center rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-white"
-        >
-        <p className='pr-3'>Search</p>
-        <MagnifyingGlassIcon className="h-4 w-4 text-gray-600" aria-hidden="true" />
+          className="flex items-center justify-center w-full md:w-auto rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-white"
+      >
+          <div className='flex'>
+            <p className='pr-3 md:pr-3'>Search</p>
+            <MagnifyingGlassIcon className="h-4 w-4 text-gray-600" aria-hidden="true" />
+          </div>
       </button>
 
       <SearchWizard isOpen={searchOpen} onClose={() => setSearchOpen(false)} />
