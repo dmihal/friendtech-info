65028e8f41f40831f5805e0e212392ed655cf087
Kalle Moen <hello@kallemoen.com>
2023-08-22 18:31:14 +0100

    Fixed table responsiveness

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,19 +1,19 @@
 'use client'
-import { FullPosition } from '@/api';
-import { useContext } from 'react';
+import { SimpleAccountData } from '@/api';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { useContext } from 'react';
 import { AppRouterContext } from 'next/dist/shared/lib/app-router-context';
 
 function classNames(...classes: (false | null | undefined | string)[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function ShareholderTable({ shareholders }: { shareholders: FullPosition[] }) {
-  let i = 1;
+export default function UserTable({ people }: { people: SimpleAccountData[] }) {
 
   const router = useContext(AppRouterContext)!
-
-  const sortedShareholders = shareholders.sort((a, b) => b.shares - a.shares)
+  let i = 1;
 
   return (
     <div className="px-4 sm:px-6 lg:px-8">
@@ -45,22 +45,22 @@ export default function ShareholderTable({ shareholders }: { shareholders: FullP
                     scope="col"
                     className="sticky top-0 z-10 border-b border-gray-300 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter hidden sm:table-cell"
                   >
-                    Shareholder Price
+                    Market cap
                   </th>
                   <th
                     scope="col"
                     className="sticky top-0 z-10 border-b border-gray-300 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                   >
-                    Shares
+                    Price
                   </th>
                 </tr>
               </thead>
               <tbody>
-                {sortedShareholders.map((shareholder, shareholderIdx) => (
-                  <tr key={shareholder.owner.id} onClick={() => router.push(`/${shareholder.owner.id}`)} className="cursor-pointer hover:bg-gray-100">
+                {people.map((person, personIdx) => (
+                  <tr key={person.id} onClick={() => router.push(`/${person.id}`)} className="cursor-pointer hover:bg-gray-100">
                     <td
                       className={classNames(
-                        shareholderIdx !== sortedShareholders.length - 1 ? 'border-b border-gray-200' : '',
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
                         'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8'
                       )}
                     >
@@ -68,40 +68,40 @@ export default function ShareholderTable({ shareholders }: { shareholders: FullP
                     </td>
                     <td
                       className={classNames(
-                        shareholderIdx !== sortedShareholders.length - 1 ? 'border-b border-gray-200' : '',
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
                         'whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:table-cell'
                       )}
                     >
                       <div className="flex items-center gap-x-4">
-                      <img src={shareholder.owner.twitterPfpUrl} alt="" className="h-8 w-8 rounded-full bg-gray-100" />
-                      <Link href={`/${shareholder.owner.id}`}>
-                        <div className="truncate text-sm font-medium leading-6 text-gray-900 w-24 md:w-auto">{shareholder.owner.twitterName || shareholder.owner.id}</div>
-                      </Link>
+                        <img src={person.twitterPfpUrl} alt="" className="h-8 w-8 rounded-full bg-gray-100" />
+                        <Link href={`/${person.id}`}>
+                          <div className="truncate text-sm font-medium leading-6 text-gray-900 w-24 md:w-auto">{person.twitterName}</div>
+                        </Link>
                       </div>
                     </td>
                     <td
                       className={classNames(
-                        shareholderIdx !== sortedShareholders.length - 1 ? 'border-b border-gray-200' : '',
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
                         'whitespace-nowrap hidden px-3 py-4 text-sm text-gray-500 lg:table-cell'
                       )}
                     >
-                      <div className="font-mono text-sm leading-6 text-gray-900">{shareholder.owner.holderCount}</div>
+                      {person.holderCount}
                     </td>
                     <td
                       className={classNames(
-                        shareholderIdx !== sortedShareholders.length - 1 ? 'border-b border-gray-200' : '',
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
                         'whitespace-nowrap hidden sm:table-cell px-3 py-4 text-sm text-gray-500'
                       )}
                     >
-                      {parseFloat(shareholder.owner.lastTradePrice).toFixed(3) + ' ETH'}
+                      {(parseFloat(person.holderCount) * (parseFloat(person.displayPrice) / 1e18)).toFixed(3) + ' ETH'}
                     </td>
                     <td
                       className={classNames(
-                        shareholderIdx !== sortedShareholders.length - 1 ? 'border-b border-gray-200' : '',
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
                         'relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-8 lg:pr-8'
                       )}
                     >
-                      {shareholder.shares + ' owned'}
+                      {(parseFloat(person.displayPrice) / 1e18).toFixed(3) + ' ETH'}
                     </td>
                   </tr>
                 ))}
@@ -113,3 +113,6 @@ export default function ShareholderTable({ shareholders }: { shareholders: FullP
     </div>
   )
 }
+
+
+
