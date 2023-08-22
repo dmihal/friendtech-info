4307ee89f30e59d93127a6dc2844062081dc41a8
Kalle Moen <hello@kallemoen.com>
2023-08-22 18:33:30 +0100

    Fixed css

--- src/app/[address]/ShareholderTable.tsx
+++ src/app/[address]/ShareholderTable.tsx
@@ -49,7 +49,7 @@ export default function ShareholderTable({ shareholders }: { shareholders: FullP
                   </th>
                   <th
                     scope="col"
-                    className="sticky top-0 z-10 border-b border-gray-300 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                    className="sticky top-0 z-10 border-b border-gray-300 bg-opacity-75 px-3 py-3.5 text-right text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                   >
                     Shares
                   </th>
